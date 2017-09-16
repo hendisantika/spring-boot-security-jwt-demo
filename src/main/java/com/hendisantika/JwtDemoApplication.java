32ecb8864ac09a0c8fb673cdaa811486d44b0e16
Hendi Santika <hendisantika@gmail.com>
2017-09-16 21:14:43 +0700

    #4 - Added Main Class

---
+++ src/main/java/com/hendisantika/JwtDemoApplication.java
@@ -0,0 +1,22 @@
+package com.hendisantika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * Created by IntelliJ IDEA.
+ * Project : customer
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 9/16/17
+ * Time: 7:06 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+@SpringBootApplication
+public class JwtDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtDemoApplication.class, args);
+    }
+}
