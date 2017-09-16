efa83114f1c77e860f9c3d41601afae77cfc3487
Hendi Santika <hendisantika@gmail.com>
2017-09-17 06:12:58 +0700

    #8 - Added Authority Name

---
+++ src/main/java/com/hendisantika/model/security/AuthorityName.java
@@ -0,0 +1,15 @@
+package com.hendisantika.model.security;
+/**
+ * Created by IntelliJ IDEA.
+ * Project : cspring-boot-security-jwt-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 9/16/17
+ * Time: 7:06 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+public enum AuthorityName {
+    ROLE_USER, ROLE_ADMIN
+}
