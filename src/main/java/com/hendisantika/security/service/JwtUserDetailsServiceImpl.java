a979b96ca4befce2ee79e9e458108c03ba67ec22
Hendi Santika <hendisantika@gmail.com>
2017-09-19 06:01:47 +0700

    #20 - JWT User Details Service Impl

---
+++ src/main/java/com/hendisantika/security/service/JwtUserDetailsServiceImpl.java
@@ -0,0 +1,38 @@
+package com.hendisantika.security.service;
+
+import com.hendisantika.model.security.User;
+import com.hendisantika.security.JwtUserFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import com.hendisantika.security.repository.UserRepository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-security-jwt-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 9/19/17
+ * Time: 5:56 PM
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+public class JwtUserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+
+        if (user == null) {
+            throw new UsernameNotFoundException(String.format("No user found with username '%s'.", username));
+        } else {
+            return JwtUserFactory.create(user);
+        }
+    }
+}
