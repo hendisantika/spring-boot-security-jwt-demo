7884ed410b707b23154420df7e7fd81dcca3fce9
Hendi Santika <hendisantika@gmail.com>
2017-09-18 20:57:59 +0700

    #18 - Added JWT User Factory

---
+++ src/main/java/com/hendisantika/security/JwtUserFactory.java
@@ -0,0 +1,45 @@
+package com.hendisantika.security;
+
+import com.hendisantika.model.security.Authority;
+import com.hendisantika.model.security.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.List;
+import java.util.stream.Collectors;
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-security-jwt-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 9/17/17
+ * Time: 7:46 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+public final class JwtUserFactory {
+
+    private JwtUserFactory() {
+    }
+
+    public static JwtUser create(User user) {
+        return new JwtUser(
+                user.getId(),
+                user.getUsername(),
+                user.getFirstname(),
+                user.getLastname(),
+                user.getEmail(),
+                user.getPassword(),
+                mapToGrantedAuthorities(user.getAuthorities()),
+                user.getEnabled(),
+                user.getLastPasswordResetDate()
+        );
+    }
+
+    private static List<GrantedAuthority> mapToGrantedAuthorities(List<Authority> authorities) {
+        return authorities.stream()
+                .map(authority -> new SimpleGrantedAuthority(authority.getName().name()))
+                .collect(Collectors.toList());
+    }
+}
