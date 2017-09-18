4f94a0ad18958905fc94942e7b4101d05a9390d3
Hendi Santika <hendisantika@gmail.com>
2017-09-19 06:04:56 +0700

    #23 - Added User Rest Controller

---
+++ src/main/java/com/hendisantika/security/controller/UserRestController.java
@@ -0,0 +1,43 @@
+package com.hendisantika.security.controller;
+
+import com.hendisantika.security.JwtUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import com.hendisantika.security.JwtTokenUtil;
+
+import javax.servlet.http.HttpServletRequest;
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-security-jwt-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 9/19/17
+ * Time: 6:02 PM
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController
+public class UserRestController {
+
+    @Value("${jwt.header}")
+    private String tokenHeader;
+
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @RequestMapping(value = "user", method = RequestMethod.GET)
+    public JwtUser getAuthenticatedUser(HttpServletRequest request) {
+        String token = request.getHeader(tokenHeader).substring(7);
+        String username = jwtTokenUtil.getUsernameFromToken(token);
+        JwtUser user = (JwtUser) userDetailsService.loadUserByUsername(username);
+        return user;
+    }
+
+}
