92db4559ab578ced6602dd2b226a7cdeceeabcfe
Hendi Santika <hendisantika@gmail.com>
2017-09-18 07:01:25 +0700

    #14 - Added JWT Authentication Request

---
+++ src/main/java/com/hendisantika/security/JwtAuthenticationRequest.java
@@ -0,0 +1,47 @@
+package com.hendisantika.security;
+
+import java.io.Serializable;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-security-jwt-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 9/17/17
+ * Time: 7:16 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+public class  JwtAuthenticationRequest implements Serializable {
+
+    private static final long serialVersionUID = -8445943548965154778L;
+
+    private String username;
+    private String password;
+
+    public JwtAuthenticationRequest() {
+        super();
+    }
+
+    public JwtAuthenticationRequest(String username, String password) {
+        this.setUsername(username);
+        this.setPassword(password);
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
