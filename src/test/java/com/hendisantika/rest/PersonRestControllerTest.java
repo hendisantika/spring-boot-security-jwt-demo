7e0bbf88641a209b784b0290e5ce1b30a3e55db4
Hendi Santika <hendisantika@gmail.com>
2017-09-19 22:00:59 +0700

    #28 - Added Person Rest Controller Test

---
+++ src/test/java/com/hendisantika/rest/PersonRestControllerTest.java
@@ -0,0 +1,66 @@
+package com.hendisantika.rest;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import com.hendisantika.security.JwtTokenUtil;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-security-jwt-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 9/18/17
+ * Time: 7:46 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PersonRestControllerTest {
+
+    private MockMvc mvc;
+
+    @MockBean
+    private JwtTokenUtil jwtTokenUtil;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Before
+    public void setup() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
+    @Test
+    public void shouldGetUnauthorizedWithoutRole() throws Exception{
+
+        this.mvc.perform(get("/persons"))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithMockUser(roles = "USER")
+    public void getPersonsSuccessfullyWithUserRole() throws Exception{
+
+        this.mvc.perform(get("/persons"))
+                .andExpect(status().is2xxSuccessful());
+    }
+
+}
+
